a06c31ca12cf523cdf9eae42dc2808e32be7de08
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-22 18:01:10 +0200

    costumizzare theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    // come costumizzare i colori
+    // colors: {
+    //   primary: "#259c9d",
+    //   secondary: "#ffa500",
+    // },
+    // backgroundColor: {
+    //   primary: "#850001",
+    //   secondary: "#ff9598",
+    // },
+    // screens:{
+
+    // }
+    // ....
   },
   plugins: [],
 };
