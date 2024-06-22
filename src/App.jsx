df69b18cf073363b727f7a03ab13e19bd58dd26e
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-22 16:22:35 +0200

    width, height, margin, padding, text, background, container, rounded, box shadow

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ function App() {
 
   return (
     <>
-      <h1 className="text-center text-5xl font-semibold text-orange-500 bg-gray-700">
+    {/* width - height - margin - padding - text - font - text color - background color */}
+      <h1 className="w-1/2 h-28 m-12 p-24 text-center text-5xl font-semibold text-orange-500 bg-gray-700">
         Hello world!
       </h1>
-      <h2 className="text-center font-bold text-2xl">Our products</h2>
+      {/* container-round-box shadow */}
+      <div className="container">
+        <h2 className="rounded shadow text-center font-bold text-2xl bg-gray-300">Our products</h2>
+      </div>
     </>
   )
 }
