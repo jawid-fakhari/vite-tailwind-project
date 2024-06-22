5d9cb57f75f5b6fc705ddfc0b7a0ccb0bd83f269
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-22 16:06:04 +0200

    vite project, tailwind installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-center text-5xl font-semibold text-orange-500 bg-gray-700">
+        Hello world!
+      </h1>
+      <h2 className="text-center font-bold text-2xl">Our products</h2>
+    </>
+  )
+}
+
+export default App
